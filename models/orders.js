ee084d8a398e1fca0027e2ae433c61b263120ef2
camilovce <milovlbc@gmail.com>
2020-10-16 01:46:10 -0500

    Delete and update almost done

--- models/orders.js
+++ models/orders.js
@@ -2,48 +2,62 @@ const { db, querySelector } = require('../config/database');
 const { verifyToken, checkAdminOrId, get } = require('../models/users');
 const moment = require('moment');
 
-/**
- * Get all orders.
- * @returns {Array} Orders array.
- */
+
+
+// ********************GET ORDER ***************************************
+
 const getOrders = async () => {
-    const query = `
-        SELECT  state, time, payment, nombreProducto FROM ordenes 
-        JOIN productosOrdenes ON ordenes.id = productosOrdenes.idOrdenes 
-        JOIN productos ON productosOrdenes.idProductos = productos.id 
-    `;
+    // const query = `
+    //     SELECT  state, time, payment, nombreProducto FROM ordenes 
+    //     JOIN productosOrdenes ON ordenes.id = productosOrdenes.idOrdenes 
+    //     JOIN productos ON productosOrdenes.idProductos = productos.id 
+    // `;
+    const query = `SELECT * FROM ordenes`;
     const result = await querySelector(query, true);
     return result[0];
 }
 
-const getOrderById = async (id) => {
-    const query = `
-    SELECT  state, time, payment, nombreProducto FROM ordenes 
-    JOIN productosOrdenes ON ordenes.id = productosOrdenes.idOrdenes 
-    JOIN productos ON productosOrdenes.idProductos = productos.id 
-        WHERE ordenes.id = '2';
-    `;
+const getOrders = async (req, res, next) => {
+    try {
+        const orders = await orderServices.getOrders();
+
+        res.status(200).json({ data: orders });
+    } catch (error) {
+        next(error);
+    }
+}
+
+const getOrderByIdQuery = async (id) => {
+    // const query = `
+    // SELECT  state, time, payment, nombreProducto FROM ordenes 
+    // JOIN productosOrdenes ON ordenes.id = productosOrdenes.idOrdenes 
+    // JOIN productos ON productosOrdenes.idProductos = productos.id 
+    //     WHERE ordenes.id = :id;
+    // `;
+    const query = `SELECT * FROM ordenes
+                   WHERE ordenes.id = :id;`;
     const result = querySelector(query, true, { id });
     return result[0];
 }
 
-const addProduct = async (req, res) => {
-    const orderProductQuery = `
-    INSERT INTO productosOrdenes (idOrdenes, idProducto)
-    VALUES (:idOrdenes, :idProducto)`;
 
-    ` SELECT af.feature_name
-FROM AutoFeature af
-INNER JOIN Auto2AutoFeature a2af
-ON af.auto_feature_id = a2af.auto_feature_id
-WHERE a2af.auto_id = 7;`
-        ` SELECT af.feature_name
-FROM AutoFeature af
-INNER JOIN Auto2AutoFeature a2af
-ON af.auto_feature_id = a2af.auto_feature_id
-WHERE a2af.auto_id = 7;`
+const getOrderById = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const order = await getOrderByIdQuery(id);
+        res.status(200).json({ data: order });
+    } catch (error) {
+        next(error);
+    }
 }
-
+/*  */// ********************GET ORDER ***************************************
+/* *
+*
+*
+*
+***
+**/
+// ********************CREATE ORDER ***************************************
 
 const descriptionCreator = async (productsiDsArray) => {
     let orderDescription = '';
@@ -60,8 +74,6 @@ const descriptionCreator = async (productsiDsArray) => {
     return [orderDescription, iDsNoRepeated];
 };
 
-
-
 const createOrder = async (iDUser, orderBody) => {
     orderBody.userId = iDUser;
     const [orderDescription, iDsNoRepeated] = await descriptionCreator(orderBody.products);
@@ -71,8 +83,8 @@ const createOrder = async (iDUser, orderBody) => {
     orderBody.time = moment().format('LT');
     console.log(orderBody.description);
     const query = `
-        INSERT INTO ordenes (time, description, payment, userId, total)
-        VALUES (:time, :description, :payment, :userId, :total);
+        INSERT INTO ordenes (time, description, payment, userId, total,address)
+        VALUES (:time, :description, :payment, :userId, :total,address);
     `;
 
     const result = await querySelector(query, false, orderBody);
@@ -94,53 +106,91 @@ const createOrder = async (req, res, next) => {
     (time, description, payment, userId, total)
     const { time, description, payment, total, products } = req.body;
 
-    if (description && time && state && wayToPay && total && products) {
+    if (description && time && payment && total && products) {
         try {
-            const orderId = await orderServices.createOrder(id, req.body);
-
+            const orderId = await createOrder(id, req.body);
             res.status(201).json({ data: orderId });
         } catch (error) {
             next(error);
         }
     } else {
-        res.status(400).json({ message: 'Malformed body request' });
+        res.status(400).json({ message: 'Missing some information!' });
+    }
+}
+// ********************CREATE ORDER ***************************************
+/* 
+*
+*
+*
+*
+*
+* */
+
+
+/**
+ ********************UPDATE ORDER***************************************
+ */
+const updateOrder = async (id, orderBody) => {
+    const { state } = orderBody;
+    const order = await getOrderById(id);
+    const [orderDescription] = await descriptionCreator(order.products);
+    const query = `
+        UPDATE ordenes
+        SET description = '${orderDescription || order.description}',
+        state = '${state || order.state}'
+        WHERE id = :id`;
+
+    await querySelector(query, false, { id });
+}
+
+const updateOrder = async (req, res, next) => {
+    const orderBody = req.body;
+    const { id } = req.params;
+
+    try {
+        await updateOrder(id, orderBody);
+        let newState = await getOrderById(id);
+        res.status(200).json({ data: id, newState: newState.state });
+    } catch (error) {
+        next(error);
+    }
+}
+/**
+ ********************UPDATE ORDER***************************************
+ *
+ * *
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
+
+/**
+ ********************DELETE ORDER***************************************
+ */
+const deleteO = async (id) => {
+    const query = 'DELETE FROM ResOrder WHERE id = :id';
+    await database.executeQuery(query, false, { id });
+}
+
+const deleteOrder = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        await deleteO(id);
+        res.status(200).json({ delete: `Order number ${id} was succesfully delted!` })
+    } catch (error) {
+        next(error);
     }
 }
 
 /**
- * Update the order by the given id.
- * @param {String} id Order id.
- * @param {Object} orderProps 
- */
-const updateOrder = async (id, orderProps) => {
-    const { description, state } = orderProps;
-    const order = await getOrderById(id);
+ ********************DELETE ORDER***************************************
+*
+**/
 
-    const query = `
-        UPDATE ResOrder
-        SET description = '${description || order.description}',
-        state = '${state || order.state}'
-        WHERE id = :id
-    `;
 
-    await database.executeQuery(query, false, { id });
-}
 
-/**
- * Delete an order with the given id.
- * @param {String} id Order id.
- */
-const deleteOrder = async (id) => {
-    const query = 'DELETE FROM ResOrder WHERE id = :id';
-
-    await database.executeQuery(query, false, { id });
-}
-
-/**
- * Get all orders from the given user.
- * @param {String} userId User id.
- * @returns {Array} Orders array.
- */
 const getUserOrders = async (userId) => {
     const query = 'SELECT * FROM ResOrder WHERE userId = :userId';
     const result = await database.executeQuery(query, true, { userId });
@@ -180,108 +230,7 @@ module.exports = {
 // ******************************************************
 // ******************************************************
 
-const orderServices = require('../services/orderServices');
 
-/**
- * Get Orders from Database and return an array of Orders.
- * @param {import('express').Request} req Request object
- * @param {import('express').Response} res Response object
- * @param {import('express').NextFunction} next Next function
- */
-const getOrders = async (req, res, next) => {
-    try {
-        const orders = await orderServices.getOrders();
-
-        res.status(200).json({ data: orders });
-    } catch (error) {
-        next(error);
-    }
-}
-
-/**
- * Get order by id.
- * @param {import('express').Request} req Request object
- * @param {import('express').Response} res Response object
- * @param {import('express').NextFunction} next Next function
- */
-const getOrderById = async (req, res, next) => {
-    const { id } = req.params;
-
-    try {
-        const order = await orderServices.getOrderById(id);
-
-        res.status(200).json({ data: order });
-    } catch (error) {
-        next(error);
-    }
-}
-
-/**
- * Create a new order.
- * @param {import('express').Request} req Request object
- * @param {import('express').Response} res Response object
- * @param {import('express').NextFunction} next Next function
- */
-const createOrder = async (req, res, next) => {
-    const { id } = req.userData;
-    const { description, time, state, wayToPay, total, products } = req.body;
-
-    if (description && time && state && wayToPay && total && products) {
-        try {
-            const orderId = await orderServices.createOrder(id, req.body);
-
-            res.status(201).json({ data: orderId });
-        } catch (error) {
-            next(error);
-        }
-    } else {
-        res.status(400).json({ message: 'Malformed body request' });
-    }
-}
-
-/**
- * Update an order.
- * @param {import('express').Request} req Request object
- * @param {import('express').Response} res Response object
- * @param {import('express').NextFunction} next Next function
- */
-const updateOrder = async (req, res, next) => {
-    const orderProps = req.body;
-    const { id } = req.params;
-
-    try {
-        await orderServices.updateOrder(parseInt(id), orderProps);
-
-        res.status(200).json({ data: id });
-    } catch (error) {
-        next(error);
-    }
-}
-
-/**
- * Delete an order.
- * @param {import('express').Request} req Request object
- * @param {import('express').Response} res Response object
- * @param {import('express').NextFunction} next Next function
- */
-const deleteOrder = async (req, res, next) => {
-    const { id } = req.params
-
-    try {
-        await orderServices.deleteOrder(id);
-
-        res.status(200).json({ data: id });
-    } catch (error) {
-        next(error);
-    }
-}
-
-/**
- * Get orders from the given user.
- * @param {import('express').Request} req Request object
- * @param {import('express').Response} res Response object
- * @param {import('express').NextFunction} next Next function
- */
 const getUserOrders = async (req, res, next) => {
     const { id } = req.params;
 
@@ -294,12 +243,7 @@ const getUserOrders = async (req, res, next) => {
     }
 }
 
-/**
- * Get all products from the given order.
- * @param {import('express').Request} req Request object
- * @param {import('express').Response} res Response object
- * @param {import('express').NextFunction} next Next function
- */
+
 const getOrderProducts = async (req, res, next) => {
     const { id } = req.params;
 
@@ -312,12 +256,6 @@ const getOrderProducts = async (req, res, next) => {
     }
 }
 
-/**
- * Middleware to verify if a product already exists.
- * @param {import('express').Request} req Request object
- * @param {import('express').Response} res Response object
- * @param {import('express').NextFunction} next Next function
- */
 const verifyIfOrderExists = async (req, res, next) => {
     const { id } = req.params;
 
